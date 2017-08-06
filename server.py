17907d5940d8ae18bc678a85f635beff2cca5f05
WEIZIBIN <1999597@qq.com>
2017-08-06 23:35:40 +0800

    update to flask

--- server.py
+++ server.py
@@ -1,109 +1,22 @@
-from http.server import HTTPServer, BaseHTTPRequestHandler
-from os import curdir, sep
-import cgi
-import json
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
 
 
-class MyHandler(BaseHTTPRequestHandler):
-
-    def do_GET(self):
-        if self.path == '/':
-            self.path = '/chat.html'
-
-        prefix = '/webpage'
-        self.path = prefix + self.path
-
-        try:
-            # Check the file extension required and
-            # set the right mime type
-
-            sendReply = False
-            if self.path.endswith('.html'):
-                mimetype = 'text/html'
-                sendReply = True
-            if self.path.endswith('.jpg'):
-                mimetype = 'image/jpg'
-                sendReply = True
-            if self.path.endswith('.gif'):
-                mimetype = 'image/gif'
-                sendReply = True
-            if self.path.endswith('.js'):
-                mimetype = 'application/javascript'
-                sendReply = True
-            if self.path.endswith('.css'):
-                mimetype = 'text/css'
-                sendReply = True
-
-            if sendReply == True:
-                # Open the static file requested and send it
-                f = open(curdir + sep + self.path, 'rb')
-                self.send_response(200)
-                self.send_header('Content-type', mimetype)
-                self.end_headers()
-                self.wfile.write(f.read())
-                f.close()
-            return
-
-        except IOError:
-            self.send_error(404, 'Not Found: %s' % self.path)
-
-    def do_POST(self):
-
-        sendReply = False
-
-        # url: /xiaoice
-        if self.path == '/xiaoice':
-            ctype, pdict = cgi.parse_header(self.headers['content-type'])
-
-            if ctype == 'application/x-www-form-urlencoded':
-                length = int(self.headers['content-length'])
-                post_values = self.rfile.read(length)
-                sendReply = True
-
-            try:
-
-                if sendReply:
-                    self.send_response(200)
-                    self.send_header('Content-type', 'application/json')
-                    self.end_headers()
-                    response = {'retCode':'0'}
-                    response_json = json.dumps(response)
-                    self.wfile.write(bytes(response_json, 'UTF8'))
-
-            except IOError:
-                self.send_error(404, 'Not Found: %s' % self.path)
-
-        # url: /im
-        if self.path == '/im':
-            ctype, pdict = cgi.parse_header(self.headers['content-type'])
-
-            if ctype == 'application/x-www-form-urlencoded':
-                length = int(self.headers['content-length'])
-                post_values = self.rfile.read(length)
-                sendReply = True
-                print(post_values)
-
-            try:
-                if sendReply:
-                    self.send_response(200)
-                    self.send_header('Content-type', 'application/json')
-                    self.end_headers()
-                    response = {'retCode': '0', 'msg': 'Send msg to talk with xiaoice!'}
-                    response_json = json.dumps(response)
-                    self.wfile.write(bytes(response_json, 'UTF8'))
-
-            except IOError:
-                self.send_error(404, 'Not Found: %s' % self.path)
+@app.route('/')
+def index():
+    return render_template('chat.html')
 
 
-def run(server_class=HTTPServer, handler_class=MyHandler):
-    server_address = ('', 8000)
-    httpd = server_class(server_address, handler_class)
-    httpd.serve_forever()
+@app.route('/xiaoice', methods=['POST'])
+def xiaoice():
+    return jsonify(retCode='0')
 
 
-def main():
-    run()
+@app.route('/im', methods=['POST'])
+def im():
+    return jsonify(retCode='0', msg='helloworld')
+
 
 if __name__ == '__main__':
-    main()
+    app.run()
