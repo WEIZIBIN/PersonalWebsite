3599043cb0258ce3d22ae3f59e713dbe50a03678
WEIZIBIN <1999597@qq.com>
2017-09-06 23:41:56 +0800

    fix wrong load order

--- flask_website/views/__init__.py
+++ flask_website/views/__init__.py
@@ -1,5 +1,5 @@
-from flask_website.views.chat import chat
-from flask_website.views.general import general
 from flask_website.views.admin import admin
 from flask_website.views.xiaoice import xiaoice
 from flask_website.views.xiaoice import dict_xiaoice
+from flask_website.views.general import general
+from flask_website.views.chat import chat
